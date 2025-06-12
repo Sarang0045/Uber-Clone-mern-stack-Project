c8ac452eeea2ebd70d1016b4f48c87b7477cb84a
sarang <sarangchoudhary0045@gmail.com>
2025-06-12 21:43:57 +0530

    .

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,19 +3,13 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
-import UserContext from './Context/UserContext.jsx'
-import CaptainContext from './Context/CaptainContext.jsx'
 import SocketProvider from './Context/SocketContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <SocketProvider>
-        <CaptainContext>
-          <UserContext>
-            <App />
-          </UserContext>
-        </CaptainContext>
+        <App />
       </SocketProvider>
     </BrowserRouter>
   </StrictMode>
