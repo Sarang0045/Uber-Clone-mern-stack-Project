ad12e1ab1a6aa0832258e81f79b7947c224b97a4
sarang <sarangchoudhary0045@gmail.com>
2025-06-12 21:40:07 +0530

    .

--- Frontend/src/Context/userContext.jsx
+++ Frontend/src/Context/userContext.jsx
@@ -2,23 +2,19 @@ import React, { createContext, useState } from 'react'
 
 export const UserDataContext = createContext()
 
-
 const UserContext = ({ children }) => {
-
     const [ user, setUser ] = useState({
         email: '',
-        fullName: {
-            firstName: '',
-            lastName: ''
+        fullname: {
+            firstname: '',
+            lastname: ''
         }
     })
 
     return (
-        <div>
-            <UserDataContext.Provider value={{ user, setUser }}>
-                {children}
-            </UserDataContext.Provider>
-        </div>
+        <UserDataContext.Provider value={{ user, setUser }}>
+            {children}
+        </UserDataContext.Provider>
     )
 }
 
