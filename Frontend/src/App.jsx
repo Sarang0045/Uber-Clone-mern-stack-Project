a327ab144477893cb5e0d1dfa59eb808d2d0c47f
sarang <sarangchoudhary0045@gmail.com>
2025-06-13 11:27:21 +0530

    .

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,22 +1,22 @@
 import React, { useContext } from "react";
 import { Route, Routes } from "react-router-dom";
 import Start from "./Pages/Start.jsx";
-import UserLogin from "./Pages/UserLogin.jsx";
+import UserLogin from "./Pages/userLogin.jsx";
 import UserSignUp from "./Pages/UserSignUp.jsx";
-import Captainlogin from "./Pages/CaptionLogin.jsx";
-import CaptainSignup from "./Pages/CaptainSignUp.jsx";
-import Home from "./Pages/Home.jsx";
+import Captainlogin from "./Pages/captionLogin.jsx";
+import CaptainSignup from "./Pages/captainSignUp.jsx";
+import Home from "./Pages/home.jsx";
 import UserContext from "./Context/UserContext.jsx";
 import UserProtectedWaper from "./Pages/UserProtectedWaper.jsx";
-import CaptainProtectWraper from "./Pages/CaptainProtectWraper.jsx";
+import CaptainProtectWraper from "./Pages/captainProtectWraper.jsx";
 import UserLogout from "./Pages/UserLogout.jsx";
-import CaptainContext from "./Context/CaptainContext.jsx";
-import CaptainHome from "./Pages/CaptainHome.jsx";
+import CaptainHome from "./Pages/captainHome.jsx";
 import CaptainLogout from "./Pages/CaptainLogout.jsx";
-import Riding from "./Pages/Riding.jsx";
-import CaptainRiding from "./Pages/CaptainRiding.jsx";
+import Riding from "./Pages/riding.jsx";
+import CaptainRiding from "./Pages/captainRiding.jsx";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import CaptainContext from "./Context/CaptainContext.jsx";
 
 const App = () => {
   return (
