5d3f7556ce9f89495c654d98fc7edfed29841f7c
sarang <sarangchoudhary0045@gmail.com>
2025-06-14 10:28:02 +0530

    .

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -4,19 +4,17 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom';
 import UserContext from './Context/UserContext.jsx';
-import CaptainContext from './Context/CapatainContext.jsx';
+import CaptainContext from './Context/CaptainContext.jsx';
 import SocketProvider from './Context/SocketContext.jsx';
 
 createRoot(document.getElementById('root')).render(
-
   <CaptainContext>
     <UserContext>
       <SocketProvider>
-        <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+        <BrowserRouter>
           <App />
         </BrowserRouter>
       </SocketProvider>
     </UserContext>
   </CaptainContext>
-
 )
