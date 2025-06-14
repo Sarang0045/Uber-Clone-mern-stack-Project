d15e6fb44548e06c5965d6ba96a02ff89f4e3318
sarang <sarangchoudhary0045@gmail.com>
2025-06-14 10:23:36 +0530

    .

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,73 +1,68 @@
-import React, { useContext } from "react";
+import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Start from "./Pages/Start.jsx";
-import UserLogin from "./Pages/userLogin.jsx";
-import UserSignUp from "./Pages/UserSignUp.jsx";
-import Captainlogin from "./Pages/captionLogin.jsx";
-import CaptainSignup from "./Pages/captainSignUp.jsx";
-import Home from "./Pages/home.jsx";
-import UserContext from "./Context/UserContext.jsx";
-import UserProtectedWaper from "./Pages/UserProtectedWaper.jsx";
-import CaptainProtectWraper from "./Pages/captainProtectWraper.jsx";
-import UserLogout from "./Pages/UserLogout.jsx";
-import CaptainHome from "./Pages/captainHome.jsx";
-import CaptainLogout from "./Pages/CaptainLogout.jsx";
-import Riding from "./Pages/riding.jsx";
-import CaptainRiding from "./Pages/captainRiding.jsx";
+import Start from "./pages/Start";
+import UserLogin from "./pages/UserLogin";
+import UserSignup from "./pages/UserSignup";
+import Captainlogin from "./pages/Captainlogin";
+import CaptainSignup from "./pages/CaptainSignup";
+import Home from "./pages/Home";
+import UserProtectWrapper from "./pages/UserProtectWrapper";
+import UserLogout from "./pages/UserLogout";
+import CaptainHome from "./pages/CaptainHome";
+import CaptainProtectWrapper from "./pages/CaptainProtectWrapper";
+import CaptainLogout from "./pages/CaptainLogout";
+import Riding from "./pages/Riding";
+import CaptainRiding from "./pages/CaptainRiding";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import CaptainContext from "./Context/CaptainContext.jsx";
+import "remixicon/fonts/remixicon.css";
 
 const App = () => {
   return (
-    <CaptainContext>
-      <UserContext>
-        <div>
-          <Routes>
-            <Route path="/" element={<Start />} />
-            <Route path="/login" element={<UserLogin />} />
-            <Route path="/riding" element={<Riding />} />
-            <Route path="/signup" element={<UserSignUp />} />
-            <Route path="/captain-login" element={<Captainlogin />} />
-            <Route path="/captain-signup" element={<CaptainSignup />} />
-            <Route
-              path="/home"
-              element={
-                <UserProtectedWaper>
-                  <Home />
-                </UserProtectedWaper>
-              }
-            />
-            <Route
-              path="/logout"
-              element={
-                <UserProtectedWaper>
-                  <UserLogout />
-                </UserProtectedWaper>
-              }
-            />
-            <Route
-              path="/captain-home"
-              element={
-                <CaptainProtectWraper>
-                  <CaptainHome />
-                </CaptainProtectWraper>
-              }
-            />
-            <Route
-              path="/captain-logout"
-              element={
-                <CaptainProtectWraper>
-                  <CaptainLogout />
-                </CaptainProtectWraper>
-              }
-            />
-            <Route path="/captain-riding" element={<CaptainRiding />} />
-          </Routes>
-          <ToastContainer />
-        </div>
-      </UserContext>
-    </CaptainContext>
+    <div>
+      <Routes>
+        <Route path="/" element={<Start />} />
+        <Route path="/login" element={<UserLogin />} />
+        <Route path="/riding" element={<Riding />} />
+        <Route path="/signup" element={<UserSignup />} />
+        <Route path="/captain-login" element={<Captainlogin />} />
+        <Route path="/captain-signup" element={<CaptainSignup />} />
+        <Route
+          path="/home"
+          element={
+            <UserProtectWrapper>
+              <Home />
+            </UserProtectWrapper>
+          }
+        />
+        <Route
+          path="/logout"
+          element={
+            <UserProtectWrapper>
+              <UserLogout />
+            </UserProtectWrapper>
+          }
+        />
+        <Route
+          path="/captain-home"
+          element={
+            <CaptainProtectWrapper>
+              <CaptainHome />
+            </CaptainProtectWrapper>
+          }
+        />
+        <Route
+          path="/captain-logout"
+          element={
+            <CaptainProtectWrapper>
+              <CaptainLogout />
+            </CaptainProtectWrapper>
+          }
+        />
+        <Route path="/captain-riding" element={<CaptainRiding />} />
+      </Routes>
+      <ToastContainer />
+    </div>
   );
 };
 
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,16 +1,19 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
-import SocketProvider from './Context/SocketContext.jsx'
+import { BrowserRouter } from 'react-router-dom';
+import UserContext from './Context/UserContext.jsx';
+import CaptainContext from './Context/CaptainContext.jsx';
+import SocketProvider from './Context/SocketContext.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
-      <SocketProvider>
-        <App />
-      </SocketProvider>
-    </BrowserRouter>
-  </StrictMode>
+    <CaptainContext>
+      <UserContext>
+        <SocketProvider>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </SocketProvider>
+      </UserContext>
+    </CaptainContext>
 )
